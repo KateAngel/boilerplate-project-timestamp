da28df9014b6e1af5d4f79b15f17fdfddba4dae4
Kateryna <kvkyrpa@gmail.com>
2023-10-16 14:47:24 +0200

    fix: cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const getTimestamp = date => ({
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 204}));  // some legacy browsers choke on 204
+app.use(cors());  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
